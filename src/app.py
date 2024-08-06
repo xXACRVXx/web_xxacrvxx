d7b96572cfa409eca6a4194e6c6778e96480baed
xXACRVXx <cabrahan55@gmail.com>
2024-08-06 15:12:57 +0700

    v0.22.37b

--- src/app.py
+++ src/app.py
@@ -62,7 +62,7 @@ log = logging.getLogger("WEB")
 load_dotenv("config.env")
 EMAIL_WEBMASTER = os.getenv("EMAIL_WEBMASTER")
 
-VERSION = "v0.21.34b"
+VERSION = "v0.22.37b"
 START_SERVER_TIME = time.time()
 log.info(f"SERVIDOR INICIADO EN: [{CONFIG.MY_OS}] [{VERSION}]")
 CONNECTION_TEST()
