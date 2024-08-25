f747938b3647d9e754c6cc739d0fea95a1118326
xXACRVXx <cabrahan55@gmail.com>
2024-08-26 04:50:03 +0700

    v0.79.38b

--- src/app.py
+++ src/app.py
@@ -20,7 +20,6 @@ import datetime
 import time
 import sys
 from dotenv import load_dotenv
-
 from termcolor import colored
 import logging
 import Modules.LOGGER
@@ -36,7 +35,7 @@ log = logging.getLogger("WEB")
 load_dotenv("config.env")
 EMAIL_WEBMASTER = os.getenv("EMAIL_WEBMASTER")
 
-VERSION = "v0.79.37b"
+VERSION = "v0.79.38b"
 START_SERVER_TIME = time.time()
 log.info(f"SERVIDOR INICIADO EN: [{CONFIG.MY_OS}] [{VERSION}]")
 USERPG.CONNECTION_TEST()
